0c08a2a6e573a3a7e47a36d4d5d8903f36347c5b
gbemiga97 <holuwagbemiga97@gmail.com>
2024-01-15 14:31:22 +0100

    second commit

---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{js,ts,jsx,tsx,}',
+  ],
+  theme: {
+    fontFamily: {
+      DEFAULT: 'Poppins'
+    },
+    container: {
+      padding: {
+        DEFAULT: '15px'
+      }
+    },
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '960px',
+      xl: '1200px'
+    },
+    extend: {
+      colors: {
+        primary: '#09E7B4',
+        secondary: '#FF680A'
+      },
+      backgroundImage: {
+        heroBg1: 'url(src/assets/hero/layer_1.png)',
+        heroBg2: 'url(src/assets/hero/layer_2.png)',
+        heroBg3: 'url(src/assets/hero/layer_3.png)',
+      }
+    },
+  },
+  plugins: [],
+}
+
